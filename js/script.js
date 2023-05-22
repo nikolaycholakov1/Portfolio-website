5c33504390a353ff46dce6c2e930a555ec0136ca
Nikolay Cholakov <106109220+nikolaycholakov1@users.noreply.github.com>
2023-05-22 17:29:46 +0300

    Added certificates
    Carousel for certificates from Softuni

---
+++ js/script.js
@@ -0,0 +1,228 @@
+// Light and Dark Mode
+function setLightMode() {
+  document.documentElement.style.setProperty("--bg-color", "gray");
+  document.documentElement.style.setProperty("--second-bg-color", "lightgray");
+  document.documentElement.style.setProperty("--text-color", "black");
+
+  document.querySelector(".light").style.display = "none";
+  document.querySelector(".dark").style.display = "block";
+}
+
+function setDarkMode() {
+  document.documentElement.style.setProperty("--bg-color", "#1f242d");
+  document.documentElement.style.setProperty("--second-bg-color", "#323946");
+  document.documentElement.style.setProperty("--text-color", "#fff");
+
+  // Hide dark mode button
+  document.querySelector(".dark").style.display = "none";
+  // Show light mode button
+  document.querySelector(".light").style.display = "block";
+}
+
+// Toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// Scroll sections active links
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  // Sticky navbar
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  //   remove toggle incon and navbar when click navbar link(scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// Scroll reveal
+
+ScrollReveal({
+  //   reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .skills-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+// Typed text
+const typed = new Typed(".multiple-text", {
+  strings: ["Web Developer"],
+  typeSpeed: 250,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: false,
+});
+
+// ABOUT SECTION LAPTOP BUTTONS
+const frontEndButton = document.querySelector(".frontend-button");
+const frontEndSkillsContainer = document.querySelector(".front-end-skills-box");
+
+const backEndButton = document.querySelector(".backend-button");
+const backEndSkillsContainer = document.querySelector(".back-end-skills-box");
+
+const toolsButton = document.querySelector(".tools-button");
+const toolsContainer = document.querySelector(".tools-skills-box");
+
+frontEndButton.addEventListener("click", frontEndBtnHandler);
+backEndButton.addEventListener("click", backEndBtnhandler);
+toolsButton.addEventListener("click", toolsBtnHandler);
+
+function frontEndBtnHandler() {
+  frontDisProp = getDisplayProperty(frontEndSkillsContainer);
+  backDisProp = getDisplayProperty(backEndSkillsContainer);
+  toolsDisProp = getDisplayProperty(toolsContainer);
+
+  // if (backDisProp === "grid") {
+  //   backEndSkillsContainer.style.display = "none";
+  // }
+
+  // if (toolsDisProp === "grid") {
+  //   toolsContainer.style.display = "none";
+  // }
+
+  // if (frontDisProp === "none") {
+  //   frontEndSkillsContainer.style.display = "grid";
+  // } else {
+  //   frontEndSkillsContainer.style.display = "none";
+  // }
+
+  let newProperty = changeDisplayProperty(
+    toolsDisProp,
+    toolsContainer,
+    backDisProp,
+    backEndSkillsContainer,
+    frontDisProp
+  );
+
+  frontEndSkillsContainer.style.display = newProperty;
+}
+
+function backEndBtnhandler() {
+  frontDisProp = getDisplayProperty(frontEndSkillsContainer);
+  backDisProp = getDisplayProperty(backEndSkillsContainer);
+  toolsDisProp = getDisplayProperty(toolsContainer);
+
+  // if (frontDisProp === "grid") {
+  //   frontEndSkillsContainer.style.display = "none";
+  // }
+
+  // if (toolsDisProp === "grid") {
+  //   toolsContainer.style.display = "none";
+  // }
+
+  // if (backDisProp === "none") {
+  //   backEndSkillsContainer.style.display = "grid";
+  // } else {
+  //   backEndSkillsContainer.style.display = "none";
+  // }
+  let newProperty = changeDisplayProperty(
+    frontDisProp,
+    frontEndSkillsContainer,
+    toolsDisProp,
+    toolsContainer,
+    backDisProp
+  );
+
+  backEndSkillsContainer.style.display = newProperty;
+}
+
+function toolsBtnHandler() {
+  frontDisProp = getDisplayProperty(frontEndSkillsContainer);
+  backDisProp = getDisplayProperty(backEndSkillsContainer);
+  toolsDisProp = getDisplayProperty(toolsContainer);
+
+  // if (frontDisProp === "grid") {
+  //   frontEndSkillsContainer.style.display = "none";
+  // } else if (backDisProp === "grid") {
+  //   backEndSkillsContainer.style.display = "none";
+  // }
+  // if (toolsDisProp === "none") {
+  //   toolsContainer.style.display = "grid";
+  // } else {
+  //   toolsContainer.style.display = "none";
+  // }
+
+  let newProperty = changeDisplayProperty(
+    frontDisProp,
+    frontEndSkillsContainer,
+    backDisProp,
+    backEndSkillsContainer,
+    toolsDisProp
+  );
+
+  toolsContainer.style.display = newProperty;
+}
+
+function changeDisplayProperty(
+  prop1,
+  prop1Container,
+  prop2,
+  prop2Container,
+  prop3
+) {
+  if (prop1 === "grid") {
+    prop1Container.style.display = "none";
+  } else if (prop2 === "grid") {
+    prop2Container.style.display = "none";
+  }
+  if (prop3 === "none") {
+    prop3 = "grid";
+  } else {
+    prop3 = "none";
+  }
+
+  return prop3;
+}
+
+function getDisplayProperty(container) {
+  const displayProperty = window
+    .getComputedStyle(container)
+    .getPropertyValue("display");
+
+  return displayProperty;
+}
+
+// MODAL??
+const openBtn = document.querySelector("[data-open-modal]");
+const closeBtn = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+
+openBtn.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+});
---
+++ js/slider.js
@@ -0,0 +1,92 @@
+const sliderContainer = document.querySelector(".slider-container");
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".slider-container div.arrow");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildren = [...carousel.children];
+const certificates = document.querySelectorAll(".card img");
+console.log(certificates);
+
+let isDragging = false,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+//   Get number of cards that can fit in carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to START of carousel for infinite scrolling
+carouselChildren
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+// Insert copies of the last few cards to END of carousel for infinite scrolling
+
+carouselChildren.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+//   Add event listeners for the arrow buttons to scroll the carusel left and right
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  //   Records the initial cursor and position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return; // If dragging is false, return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+const autoPlay = () => {
+  if (window.innerWidth < 800) return; //Return on smaller than 800px screen
+  //Auto play carousel every 2.5s
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2500);
+};
+
+autoPlay();
+
+const infiniteScroll = () => {
+  // If carousel is at START go to END
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.scrollWidth;
+    carousel.classList.remove("no-transition");
+  }
+  // If carousel is at END, go to START
+  else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  //Clear existing timeout and start of mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!sliderContainer.matches(":hover")) autoPlay();
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mouseover", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+sliderContainer.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+sliderContainer.addEventListener("mouseleave", autoPlay);
